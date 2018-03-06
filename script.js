6de03d7e5f0085348b65b40e7219c2949944f6d4
Cameron Buscher <cameronbuscher@gmail.com>
2018-03-05 23:26:52 -0700

    Load and delete functionality
    Can now load all items in storage and prepend them to the page, as well as remove individual items from the page and delete them from storage.

--- script.js
+++ script.js
@@ -18,13 +18,14 @@ $('.idea-list').on('blur', 'p', editBodyText);
 $(document).on('input', '.search-input', searchIdeas);
 $(window).on('load', function() {
   loadIdeaList();
-  displayIdeas();
+  // displayIdeas();
 });
 
-function Idea(title, body) {
-  this.id = Date.now();
+function Idea(title, body, id, quality) {
+  this.id = id || Date.now();
   this.title = title || 'No title specified';
   this.body = body || 'No body specified';
+  this.quality = quality || 'swill';
 };
 
 function createIdea() {
@@ -33,10 +34,10 @@ function createIdea() {
   var newIdea = new Idea(ideaTitleInputValue, ideaBodyInputValue);
   var JSONNewIdea = JSON.stringify(newIdea);
   localStorage.setItem(newIdea.id, JSONNewIdea);
-  prependIdea(newIdea.title, newIdea.body, newIdea.id);
+  prependIdea(newIdea.title, newIdea.body, newIdea.id, newIdea.quality);
 };
 
-function prependIdea(title, body, id) {
+function prependIdea(title, body, id, quality) {
   $('.idea-list').prepend(`
     <div class="idea" id='${id}'>
       <h2 aria-label="Idea title" contenteditable="true">${title}</h2> 
@@ -47,38 +48,47 @@ function prependIdea(title, body, id) {
           <img tabindex="0" role="button" aria-label="Increase quality" class="upvote-button icon" src="icons/upvote.svg">
           <img tabindex="0" role="button" aria-label="Decrease quality" class="downvote-button icon" src="icons/downvote.svg">
         </div>
-        <p class="idea-quality-container">quality: <span class="idea-quality">swill</span></p>
+        <p class="idea-quality-container">quality: <span class="idea-quality">${quality}</span></p>
       </div>  
       <hr>
     </div>
     `);
 };
 
-function storeIdeaList() {
-  var ideaList = $('.idea-list').html();
-  var JSONIdeaList = JSON.stringify(ideaList);
-  localStorage.setItem('storedIdeaList', JSONIdeaList);
-};
+// function storeIdeaList() {
+//   var ideaList = $('.idea-list').html();
+//   var JSONIdeaList = JSON.stringify(ideaList);
+//   localStorage.setItem('storedIdeaList', JSONIdeaList);
+// };
 
 function loadIdeaList() {
-  var retrievedIdeaList = localStorage.getItem('storedIdeaList');
-  var parsedIdeaList = JSON.parse(retrievedIdeaList);
-  $('.idea-list').prepend(parsedIdeaList);
+  $.each(localStorage, function(key) {
+    var idea = localStorage.getItem(key);
+    var parsedIdea = JSON.parse(idea);
+    prependIdea(parsedIdea.title, parsedIdea.body, parsedIdea.id, parsedIdea.quality);
+  });
 };
 
 function deleteIdea() {
+  var id = $(this).closest('.idea').prop('id');
+  $.each(localStorage, function(key) {
+    if (key === id) {
+      localStorage.removeItem(key);
+    }
+  });
   $(this).closest('.idea').remove();
-  storeIdeaList();
 }
 
 function upvote() {
   var $qualityLevel = $(this).parentsUntil('.idea').find('.idea-quality').text();
+  var id = $(this).parentsUntil('.idea').prop('id');
+  var title = $(this).parentsUntil('h2').text();
+  var body = $(this).parentsUntil('p').text();
   if ($qualityLevel === 'swill') {
     $(this).parentsUntil('.idea').find('.idea-quality').text('plausible');
   } else {
     $(this).parentsUntil('.idea').find('.idea-quality').text('genius');
   }
-  storeIdeaList();
 }
 
 function downvote() {
@@ -88,7 +98,6 @@ function downvote() {
   } else {
     $(this).parentsUntil('.idea').find('.idea-quality').text('swill');
   }
-  storeIdeaList();
 };
 
 function searchIdeas() {
@@ -96,7 +105,6 @@ function searchIdeas() {
   $(".idea-list .idea").filter(function() {
   $(this).toggle($(this).text().toLowerCase().indexOf(searchValue) > -1)
   });
-  storeIdeaList();
 }
 
 function clearInputs() {
@@ -104,20 +112,18 @@ function clearInputs() {
   $ideaBodyInput.val('');
 }
 
-function displayIdeas() {
-  $('.idea').removeAttr('style');
-}
+// function displayIdeas() {
+//   $('.idea').removeAttr('style');
+// }
 
 function editTitleText() {
   var newText = $(this).text();
   console.log($(this));
   $(this).html(`${newText}`);
-  storeIdeaList();
 };
 
 function editBodyText() {
   var newText = $(this).text();
   $(this).html(`${newText}`);
-  storeIdeaList();
 };
 
